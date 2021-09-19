f56c9054e4257dad81454c04529a06410c167bb4
loisolire <loisolire@gmail.com>
2021-09-19 14:48:50 +0200

    Implement GET REST API done

---
+++ 06-context-and-rest-api/clip-03-add-insert-delete-to-request-context/pages/api/speakers/index.js
@@ -0,0 +1,28 @@
+import {data} from '../../../SpeakerData';
+import path from 'path';
+
+import { promisify } from 'util';
+import  fs from 'fs';
+
+const readFile = promisify(fs.readFile);
+
+const delay = ms => new Promise(resolve=>setTimeout(resolve,ms));
+
+const jsonFile = path.resolve('./','db.json');
+
+export default async function handler(req, res){
+    try {
+        const readFileData = await readFile(jsonFile);
+        await delay(1000);
+        const speakers = JSON.parse(readFileData).speakers;
+
+        if(speakers){
+            res.setHeader({'Content-Type': 'application/json'});
+            res.status(200).send(JSON.stringify(speakers,null,2));
+            console.log(`Get api/peakers ok !`);
+        }
+    } catch(e){
+        console.error(`Get api/speakers ko`, e);
+        res.status(404).send('response not found !')
+    }
+}
